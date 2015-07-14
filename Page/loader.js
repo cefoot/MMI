c9beab4f0e79f6a453dd2d4526048cdcc45fd74a
cefoot <cefoot@outlook.de>
2015-07-14 10:30:24 +0200

    d) Zeitlichen Text anzeigen

--- Page/loader.js
+++ Page/loader.js
@@ -1,4 +1,27 @@
-﻿var showVideo = function (file) {
+﻿String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) { hours = "0" + hours; }
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
+    var time = minutes + ':' + seconds + ':00';
+    if (hours > 0) {
+        time = hours + ':' + time;
+    }
+    return time;
+}
+
+var clearContent = function () {
+    var dataCell = document.getElementById("dataContend");
+    dataCell.innerHTML = '';
+    var timeCell = document.getElementById("timeDisplay");
+    timeCell.innerHTML = '';
+}
+
+var showVideo = function (file, detail) {
     var dataCell = document.getElementById("dataContend");
     var timeCell = document.getElementById("timeDisplay");
     var vid = document.createElement("video");
@@ -10,26 +33,41 @@
     dataCell.appendChild(vid);
     var progr = document.createElement("div");
     vid.progr = progr;
+    vid.dataDetail = detail;
+    vid.dataTimes = Object.keys(detail).sort();
     progr.className = "progress";
 
     timeCell.innerHTML = '';
     timeCell.appendChild(progr);
     progr.style.height = timeCell.offsetHeight + "px";
+    document.getElementById("timeContend").lastIdx = 0;
 
     vid.play();
     vid.ontimeupdate = function (evt, obj) {
-        var perc = evt.srcElement.currentTime / evt.srcElement.duration * 100
+        var perc = evt.srcElement.currentTime / evt.srcElement.duration * 100;
         evt.srcElement.progr.style.backgroundImage = "linear-gradient(black " + perc + "%,white " + (perc + 1) + "%)";
-        //evt.srcElement.progr.value = evt.srcElement.currentTime;
+
+        var time = evt.srcElement.currentTime.toHHMMSS()
+        var timeCont = document.getElementById("timeContend");
+        if(evt.srcElement.dataTimes[timeCont.lastIdx] < time){
+            timeCont.innerHTML = '';
+            timeCont.appendChild(document.createTextNode(evt.srcElement.dataDetail[evt.srcElement.dataTimes[timeCont.lastIdx]]));
+            timeCont.lastIdx++;
+        }
     };
 }
 
 var handleMenuClicked = function (content) {
+    clearContent();
+    if (!content || !content.type) {
+        document.getElementById("greeting").style.display = "";
+        return;
+    }
     switch (content.type) {
         case "1":
             //video
             document.getElementById("greeting").style.display = "none";
-            showVideo(content.file);
+            showVideo(content.file, content.detail);
             break;
     }
 };
